559cbf1131d7a6022aaa68d51464d854d765012b
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-12-09 11:39:35 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,10 +1,10 @@
 class Form{
     constructor(){
-       this.input = createInput("Name");
-       this.button = createButton('Play');
+       this.input = createInput("NOME");
+       this.button = createButton('Jogar');
        this.greeting = createElement('h2');
        this.title = createElement('h2');
-       this.reset = createButton('Reset');
+       this.reset = createButton('Reiniciar');
     }
     hide() {
         this.greeting.hide();
@@ -13,7 +13,7 @@ class Form{
         this.title.hide();
     }
     display() {
-        this.title.html("FRUIT CATCHER");
+        this.title.html("APANHADOR DE FRUTAS");
         this.title.position(350, 50);
         this.title.style('font-size', '70px');
         this.title.style('color', 'skyblue');
@@ -38,7 +38,7 @@ class Form{
             player.index = playerCount;
             player.update();
             player.updateCount(playerCount);
-            this.greeting.html("Hello " + player.name)
+            this.greeting.html("Olá, " + player.name)
             this.greeting.position(400,250);
             this.greeting.style('color', 'white');
             this.greeting.style('font-size', '100px');
