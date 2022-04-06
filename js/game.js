28a968a55760979b2bca91e4f6c4eed4c34f337a
manugonzalezzf <m4nu.freire@gmail.com>
2022-04-06 18:48:12 -0300

    update

--- js/game.js
+++ js/game.js
@@ -134,7 +134,7 @@ class Game{
                       if(obstacleGroup.isTouching(players)){
                        // gameState = 0;
                        // gameState = 1;
-                       // gameState = 2;
+                        gameState = 2;
                        // gameState = 3;
                       }
                   }
@@ -169,7 +169,7 @@ class Game{
     {       
             var x, y;
             //x= 200;
-            //x = random(0, width-100);
+            x = random(0, width-100);
             //x = random(0);
             //x = random();
             
